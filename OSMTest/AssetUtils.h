f25952f48e3fddb4f462d9573ac2a09fb54bbb0e
agiatdonguri <taiganishida2009@gmail.com>
2020-03-15 18:39:21 -0700

    Updated AssetUtils

--- OSMTest/AssetUtils.h
+++ OSMTest/AssetUtils.h
@@ -2,12 +2,13 @@
 
 #include "Vertex.h"
 #include <vector>
+#include <glm/glm.hpp>
 
 class AssetUtils
 {
 public:
 	static std::vector<Vertex> createSphere(float radius);
-	static std::vector<Vertex> createPrism(std::vector<std::pair<float, float>> polygon, float height);
+	static std::vector<Vertex> createPrism(const std::vector<glm::vec2>& polygon, float height);
 	static std::vector<Vertex> createRectangle(float width, float length);
 };
 
